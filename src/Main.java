895583be74ca67fa57ed5a04514808c430d289a6
Matheus Vidarte Sandoval <matheusvidartesandoval@airdematheus9.zte.com.cn>
2020-03-09 22:31:14 -0500

    primer commit nada

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import processing.core.PApplet;
+
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("Main");
+		
+		// CARGAR LAS IMAGENES
+		
+
+
+		
+		//VAR PARA PANTALLAS
+	
+
+	}
+	
+	public void settings () {
+	
+	}
+	
+	public void setup () {
+		
+	}
+	
+	public void draw () {
+
+		
+		
+	}
+
+	public void mousePressed () {
+		
+	}
+}
